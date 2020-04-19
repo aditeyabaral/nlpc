d78d07a73f48037a74ee9d8c715ac4d0a8cce65f
Aditeya Baral <33230959+aditeyabaral@users.noreply.github.com>
2020-04-19 21:39:39 +0530

    Update LevenshteinDistance.c

--- LevenshteinDistance.c
+++ LevenshteinDistance.c
@@ -1,11 +1,37 @@
-int LevenshteinDistance(char *s1, char*s2)
+int LevenshteinDistance(char *word1, char *word2)
 {
-	int len1 = strlen(s1), len2= strlen(s2);
-	int ld = fabs(len1-len2);
-	for(int i = 0; i< strlen(s2);i++)
-	{
-		if (s1[i]!=s2[i])
-			ld++;
-	}
-	return ld;
+    int n1 = strlen(word1);
+    int n2 = strlen(word2);
+    if (n1==0 && n2!=0)
+        return n2;
+    else if (n2==0 && n1!=0)
+        return n1;
+    else if (n2==0 && n1==0)
+        return 0;
+    else
+    {
+        int dp[n1+1][n2+1];
+        memset(dp,0,n1*n2);
+        for(int i=0;i<=n1;i++)
+        {
+            for(int j=0;j<=n2;j++)
+            {
+                if(i==0)
+                    dp[i][j]=j;
+                else if(j==0)
+                    dp[i][j]=i;
+                else if(word1[i-1]==word2[j-1])
+                    dp[i][j]=dp[i-1][j-1];
+                else
+                    dp[i][j]=1+min(min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1]);
+            }
+        }
+        return dp[n1][n2];
+    }
 }
+
+int min(int a,int b)
+{
+    return a<b?a:b;
+}
+
