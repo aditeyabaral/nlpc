7258915f8b85e3640e2facc289f3b27c9510c2ef
Anirudh H M <anihm136@gmail.com>
2020-10-05 13:14:01 +0530

    Allow saving of lemmatizer graph

--- include/lemmatizer.h
+++ include/lemmatizer.h
@@ -25,60 +25,124 @@ struct graph
 };
 typedef struct graph LEMMATIZER;
 
-static int hash(char a) {
+static int hash(char a)
+{
     return a - 'a';
+}
+
+static int getLength(LEMMANODE *head)
+{
+    int count = 0;
+    LEMMANODE *inc = head;
+    while (inc != NULL)
+    {
+        count++;
+        inc = inc->next;
+    }
+    return count;
 }
 
 LEMMATIZER *lemmatizer()
 {
     LEMMATIZER *graph = (LEMMATIZER *)malloc(sizeof(LEMMATIZER));
     graph->head = (FIRST *)calloc(sizeof(FIRST), 26);
-    for (int i = 0; i < 26; ++i) {
-        graph->head[i].ch = i+'a';
+    for (int i = 0; i < 26; ++i)
+    {
+        graph->head[i].ch = i + 'a';
         graph->head[i].next = NULL;
     }
-
-    FILE *fp = fopen("include/VerbForms.csv", "r");
-    char s[100], temp[100];
-    char *token, *save;
-    char x;
-    while (fgets(s, 100, fp) != NULL)
+    FILE *saved = fopen("include/lemmatizer_graph.dat", "r");
+    if (saved == NULL)
     {
-        strcpy(temp, s);
-        token = strtok_r(temp, ",", &save);
-        x = token[0];
-        LEMMANODE *l = (LEMMANODE *)malloc(sizeof(LEMMANODE));
-        l->next = l->lemma = NULL;
-        strcpy(l->word, token);
-        token = strtok_r(NULL, ",", &save);
-        while (token != NULL)
+        saved = fopen("include/lemmatizer_graph.dat", "w");
+        FILE *fp = fopen("include/VerbForms.csv", "r");
+        char s[100], temp[100];
+        char *token, *save;
+        char x;
+        while (fgets(s, 100, fp) != NULL)
         {
-            LEMMANODE *node = (LEMMANODE *)malloc(sizeof(LEMMANODE));
-            strcpy(node->word, token);
-            node->lemma = l;
-            node->next = NULL;
-            FIRST *pres = &graph->head[hash(x)];
-            if (pres->next == NULL)
-            {
-                LIST *l = (LIST *)malloc(sizeof(LIST));
-                l->head = node;
-                pres->next = node;
-            }
-            else
-            {
-                LEMMANODE *cur = pres->next;
-                LEMMANODE *prev = NULL;
-                while (cur != NULL)
-                {
-                    prev = cur;
-                    cur = cur->next;
-                }
-                prev->next = node;
-            }
+            strcpy(temp, s);
+            token = strtok_r(temp, ",", &save);
+            x = token[0];
+            LEMMANODE *l = (LEMMANODE *)malloc(sizeof(LEMMANODE));
+            l->next = l->lemma = NULL;
+            strcpy(l->word, token);
             token = strtok_r(NULL, ",", &save);
+            while (token != NULL)
+            {
+                LEMMANODE *node = (LEMMANODE *)malloc(sizeof(LEMMANODE));
+                strcpy(node->word, token);
+                node->lemma = l;
+                node->next = NULL;
+                FIRST *pres = &graph->head[hash(x)];
+                if (pres->next == NULL)
+                {
+                    LIST *l = (LIST *)malloc(sizeof(LIST));
+                    l->head = node;
+                    pres->next = node;
+                }
+                else
+                {
+                    LEMMANODE *cur = pres->next;
+                    LEMMANODE *prev = NULL;
+                    while (cur != NULL)
+                    {
+                        prev = cur;
+                        cur = cur->next;
+                    }
+                    prev->next = node;
+                }
+                token = strtok_r(NULL, ",", &save);
+            }
+        }
+        fclose(fp);
+        for (int i = 0; i < 26; ++i)
+        {
+            LEMMANODE *next = graph->head[i].next;
+            int len = getLength(next);
+            fwrite(&len, sizeof(int), 1, saved);
+            while (next != NULL)
+            {
+                int wordlen = strlen(next->word);
+                fwrite(&wordlen, sizeof(int), 1, saved);
+                fwrite(next->word, wordlen, 1, saved);
+                wordlen = strlen(next->lemma->word);
+                fwrite(&wordlen, sizeof(int), 1, saved);
+                fwrite(next->lemma->word, wordlen, 1, saved);
+                next = next->next;
+            }
         }
     }
-    fclose(fp);
+    else
+    {
+        int nextsize, wordsize;
+        for (int i = 0; i < 26; ++i)
+        {
+            fread(&nextsize, sizeof(int), 1, saved);
+            if (!nextsize)
+                continue;
+            graph->head[i].next = (LEMMANODE *)malloc(sizeof(LEMMANODE));
+            fread(&wordsize, sizeof(int), 1, saved);
+            fread(graph->head[i].next->word, wordsize, 1, saved);
+            graph->head[i].next->lemma = (LEMMANODE *)malloc(sizeof(LEMMANODE));
+            fread(&wordsize, sizeof(int), 1, saved);
+            fread(graph->head[i].next->lemma->word, wordsize, 1, saved);
+            if (!--nextsize)
+                continue;
+            LEMMANODE *next = graph->head[i].next;
+            while (nextsize--)
+            {
+                next->next = (LEMMANODE *)malloc(sizeof(LEMMANODE));
+                fread(&wordsize, sizeof(int), 1, saved);
+                fread(next->next->word, wordsize, 1, saved);
+                next->next->lemma = (LEMMANODE *)malloc(sizeof(LEMMANODE));
+                fread(&wordsize, sizeof(int), 1, saved);
+                fread(next->next->lemma->word, wordsize, 1, saved);
+                next = next->next;
+            }
+        }
+    }
+    fclose(saved);
     return graph;
 }
 
